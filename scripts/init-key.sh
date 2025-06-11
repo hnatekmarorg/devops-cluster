fb52cd39cd8138901d8b154978754fad43b3a638
Hnatekmar <m.hnatma@gmail.com>
2025-06-11 23:17:53 +0200

    fix: argocd

--- scripts/init-key.sh
+++ scripts/init-key.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl create secret generic -n sops-operator --from-file=key=~/age/key.txt  sops-age-key-file
+kubectl create secret generic -n sops --from-file=key=$(realpath ~/age/key.txt)  sops-age-key-file

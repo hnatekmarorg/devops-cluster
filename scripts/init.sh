a432b3f29eeb5a4f5a1cfafd5a586242220cca4d
Hnatekmar <m.hnatma@gmail.com>
2025-08-04 06:05:26 +0200

    fix: remove argo

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 kubectl create secret generic -n sops --from-file=key=$(realpath ~/age/key.txt)  sops-age-key-file
-kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/master/deployments/multus-daemonset-thick.yml
+#kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/master/deployments/multus-daemonset-thick.yml
